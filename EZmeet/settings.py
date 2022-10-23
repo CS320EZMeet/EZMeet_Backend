dbb8f02bfb1c7c3389418fc3f66724a085b735a0
ManayLaptop <manay.patel.1@gmail.com>
2022-10-23 00:59:27 -0400

    Got rid of obsolte package use

--- EZmeet/settings.py
+++ EZmeet/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +134,6 @@ USE_TZ = True
 #automatic configuration with django-heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
