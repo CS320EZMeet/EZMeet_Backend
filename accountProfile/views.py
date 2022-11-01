2aaa100dc62574644ec09813434fb8eec2d32b5b
Jacob Carrier <carrierjacobr@gmail.com>
2022-11-01 14:03:28 -0400

    Added createUser function template in views

--- accountProfile/views.py
+++ accountProfile/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import findUser
 import json
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 #from rest_framework import viewsets
 #from .serializers import UserSerializer
 
@@ -12,6 +12,26 @@ import json
 #welcome page
 def index(request):
     return HttpResponse("Welcome page to Account profile")
+
+# A possible template for creating a user from front-end form, and placing in DB
+def createUser(request, userObj):
+    if request.method == 'PUT':
+        if findUser(user.username):
+            return HttpResponse('That username is taken. Please try another.')
+        else:
+            user = User.objects.create_user(
+                username=userObj.username,
+                email=userObj.email,
+                password=userObj.password
+            )
+            # I think we would then want to CREATE TABLE for user - unsure how to proceed, function in models?
+            return JsonResponse(data = {'status': 200,'success': True, 'data': userObj, 'message': 'User created.'}, status = 405)
+    else:
+        return JsonResponse(data = {'status': 405,'success': False, 'message': 'This endpoint only supports PUT requests.'}, status = 405)
+
+# Should have login functionality, like createUser for sign-up
+def login(request):
+    return
 
 #does user want to make their location be private/public?
 def showLocation(request):
