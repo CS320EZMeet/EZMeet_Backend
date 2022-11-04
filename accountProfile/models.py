0671e81601dce344ae871dfff473d36ae13b4803
Jacob Carrier <carrierjacobr@gmail.com>
2022-11-04 17:16:00 -0400

    implemented basic login, and worked on user registration in both views and models files

--- accountProfile/models.py
+++ accountProfile/models.py
@@ -16,3 +16,20 @@ def findUser(userName):
         return real_dict[0]
     else:
         return None
+
+# Largely copy-pasted from above; lmk or feel free to change if part of it doesn't make sense
+def createUser(user):
+    with psycopg2.connect(user=env.USER, 
+                          password=env.PASSWORD, 
+                          host=env.HOST, 
+                          port=env.PORT, 
+                          database=env.NAME) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO users (Username, Password, Email, Group_id)) VALUES (%s, %s, %s, %s)",
+                           (user.name, user.password, user.email, user.groupId))
+            columns = [desc[0] for desc in cursor.description]
+            real_dict = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    if len(real_dict) != 0:
+        return real_dict[0]
+    else:
+        return None
--- accountProfile/views.py
+++ accountProfile/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import findUser
-import json
+from .models import findUser, createUser
+# import simplejson as json
 from django.contrib.auth.models import User
 #from rest_framework import viewsets
 #from .serializers import UserSerializer
@@ -14,28 +14,34 @@ def index(request):
     return HttpResponse("Welcome page to Account profile")
 
 # A possible template for creating a user from front-end form, and placing in DB
-def createUser(request, userObj):
+def registerUser(request, user):
     if request.method == 'PUT':
         if findUser(user.username):
-            return HttpResponse('That username is taken. Please try another.')
+            return JsonResponse(data = {'status': 409, 'success': False, 'message': 'That username is taken. Please try another.'}, status = 409)
         else:
-            user = User.objects.create_user(
-                username=userObj.username,
-                email=userObj.email,
-                password=userObj.password
-            )
-            # I think we would then want to CREATE TABLE for user - unsure how to proceed, function in models?
-            return JsonResponse(data = {'status': 200,'success': True, 'data': userObj, 'message': 'User created.'}, status = 405)
+            # django_user = User.objects.create_user(
+            #     username=userObj.username,
+            #     email=userObj.email,
+            #     password=userObj.password
+            # )
+            createUser(user)
+            return JsonResponse(data = {'status': 200,'success': True, 'data': user, 'message': 'User created.'}, status = 200)
     else:
         return JsonResponse(data = {'status': 405,'success': False, 'message': 'This endpoint only supports PUT requests.'}, status = 405)
 
 # Should have login functionality, like createUser for sign-up
-def login(request):
-    return
+def login(request, user):
+    if request.method == 'POST':
+        if findUser(user.username):
+            return JsonResponse(data = {'status': 200, 'success': True, 'data': user, 'message': 'Logged-in.'}, status = 200)
+        else:
+            return JsonResponse(data = {'status': 401, 'success': False, 'message': 'That account doesn\'t exist. Create a new account.'}, status = 401)
+    else:
+        return JsonResponse(data = {'status': 405,'success': False, 'message': 'This endpoint only supports POST requests.'}, status = 405)
 
 #does user want to make their location be private/public?
 def showLocation(request):
-    return 
+    return
 
 #user's preference list of activities they want to do
 def preferences(request):
