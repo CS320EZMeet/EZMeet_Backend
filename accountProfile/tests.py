c4456886838f2a7a4020eab3d724d56d83142ca7
Jacob Carrier <carrierjacobr@gmail.com>
2022-11-07 19:01:58 -0500

    Fixed testcases

--- accountProfile/tests.py
+++ accountProfile/tests.py
@@ -3,25 +3,26 @@ import json
 
 class accountProfileView(SimpleTestCase):
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/user/get/Yeet')
+        response = self.client.get('/user/get/Yeet/')
         self.assertEqual(response.status_code, 200)
         content = json.loads(response.content)
-        user = content['data']['name']
+        print(response)
+        user = content['data'][0]['username']
         self.assertEqual(user,'Yeet')
-        email = content['data']['email']
+        email = content['data'][0]['email']
         self.assertEqual(email,'yeet@yahoo.com')
 
     def test_login_success(self):
-        response = self.client.get('user/login/Yeet')
+        response = self.client.post('/user/login/Yeet/')
         self.assertEqual(response.status_code, 200)
         content = json.loads(response.content)
         success = content['success']
         self.assertEqual(success, True)
-        userName = content['data']['username']
+        userName = content['data'][0]['username']
         self.assertEqual(userName, 'Yeet')
     
     def test_login_incorrect_username(self):
-        response = self.client.get('user/login/eebydeeby')
+        response = self.client.post('/user/login/eebydeeby/')
         content = json.loads(response.content)
         success = content['success']
         self.assertEqual(success, False)
