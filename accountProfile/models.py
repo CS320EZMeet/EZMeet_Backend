2e78b04aea8544648be793361ef9a086d48908f4
Jacob Carrier <carrierjacobr@gmail.com>
2022-12-02 12:57:52 -0500

    Preference List part of user object

--- accountProfile/models.py
+++ accountProfile/models.py
@@ -12,10 +12,35 @@ def findUser(userName):
             cursor.execute("SELECT * FROM \"ezmeet-schema\".users WHERE username = %s", (userName,))
             columns = [desc[0] for desc in cursor.description]
             real_dict = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            cursor.execute("SELECT Preference_list_id FROM \"ezmeet-schema\".user_preferences WHERE username = %s", (userName,))
+            prefID = cursor.fetchone()[0]
     if len(real_dict) != 0:
-        return real_dict[0]
+        res = real_dict[0]
+        res['preferences'] = generatePreferenceList(prefID)
     else:
         return None
+
+def generatePreferenceList(prefID):
+    preferences = ["restaurant", "nature", "museums", "entertainment", "shopping"]
+    res = []
+    pow = 16
+    for i in range(5):
+        if prefID >= pow:
+            res.append(preferences[i])
+            prefID -= pow
+        pow /= 2
+
+    return res
+
+def generatePreferenceID(prefList):
+    preferences = ["restaurant", "nature", "museums", "entertainment", "shopping"]
+    nums = [16, 8, 4, 2, 1]
+    res = 0
+    for i in range(5):
+        if prefList.count(preferences[i]) > 0:
+            res += nums[i]
+
+    return res
 
 def createUser(user):
     with psycopg2.connect(user=env.USER, 
@@ -36,14 +61,11 @@ def updateFields(user):
                           database=env.NAME) as connection:
         with connection.cursor() as cursor:
             userName = user.userName
-            for field in user.keys():
-                cursor.execute("UPDATE \"ezmeet-schema\".users %s = %s WHERE Username = %s", (field, user['field'], userName))
-            columns = [desc[0] for desc in cursor.description]
-            real_dict = [dict(zip(columns, row)) for row in cursor.fetchall()]
-    if len(real_dict) != 0:
-        return real_dict[0]
-    else:
-        return None
+            for field in ['email', 'password', 'group_id', 'show_location']:
+                cursor.execute("UPDATE \"ezmeet-schema\".users SET %s = %s WHERE Username = %s", (field, user['field'], userName))
+            id = generatePreferenceID(user.preferences)
+            cursor.execute("UPDATE \"ezmeet-schema\".user_preferences SET preference_list_id = %d WHERE Username = %s", (id, userName))
+    return findUser(userName)
 
 def findLocation(userName):
     with psycopg2.connect(user=env.USER, 
